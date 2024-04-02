5a7bbc7497bfca9ecf183fa1abe554fdd540e934
Kaike da Silva Sathler <104788755+KaikeSathler@users.noreply.github.com>
2024-04-01 22:12:24 -0300

    update js

--- main.js
+++ main.js
@@ -21,19 +21,18 @@ function chamarAPI(cidade, carregadoInicio = false, lat = 0, long = 0) {
         const temperatura_min = parseInt(((parseInt(dados.main.temp_min) - 273)));
         const velocidade_vento = parseInt(dados.wind.speed)
         const umidade = parseInt(dados.main.humidity)
-        document.getElementById("temperatura").innerText = temperatura + " ºC";
-        document.getElementById("temperatura_max").innerText = temperatura_max + " ºC";
-        document.getElementById("temperatura_min").innerText = temperatura_min + " ºC";
-        document.getElementById("vento").innerText = velocidade_vento + " km/h";
-        document.getElementById("umidade").innerText = velocidade_vento + " %";
-        document.getElementById("cidade").innerText = dados.name + ", " + dados.sys.country;
-        if (temperatura < 20) {
-            document.getElementById("color_temp").style.backgroundColor = "rgb(255, 216, 87)";
-          } else {
-            document.getElementById("color_temp").style.backgroundColor = "rgb(255, 227, 46)";
-          }
-    
+        const iconCode = dados.weather[0].icon;
+        const isSunny = iconCode.includes('01d') || iconCode.includes('02d');
+        const color = isSunny ? 'rgb(255, 210, 97)' : 'rgb(152, 156, 156)';
+        document.getElementById("temperatura").innerHTML = temperatura + "<sup>ºC</sup>";
+        document.getElementById("temperatura_max").innerHTML = temperatura_max + "<sup>ºC</sup>";
+        document.getElementById("temperatura_min").innerHTML = temperatura_min + "<sup>ºC</sup>";
+        document.getElementById("vento").innerHTML = velocidade_vento + " km/h";
+        document.getElementById("umidade").innerHTML = velocidade_vento + " %";
+        document.getElementById("cidade").innerHTML = dados.name + ", " + dados.sys.country;
+        document.getElementById("color_temp").style.backgroundColor = color;
         document.getElementById("imagem").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}@4x.png`
+        document.getElementById("imagem").style.width = "8rem";
     }).catch((e) => {
         throw new Error(e)
     })
