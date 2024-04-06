b5de09a05061f7c43b10d3dd8f63c286fc65e3da
Kaike da Silva Sathler <104788755+KaikeSathler@users.noreply.github.com>
2024-04-06 14:51:25 -0300

    uptade js 06/04/2024,  fixes : Code optimization

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ function chamarAPI(cidade, carregadoInicio = false, lat = 0, long = 0) {
     // Verifica a URL correta para fazer a requisição. Caso carregadoInicio seja false, chama a API de puxar o clima pelo nome da cidade. Caso contrário, puxa o clima pela latitude e longitude.
     let url = "";
     if(!carregadoInicio) {
-        url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=cfcbf17c77e386b244fad41ab0dc6eec`;
+        url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cidade)}&appid=cfcbf17c77e386b244fad41ab0dc6eec&units=metric&lang=pt_br`;
     } else {
-        url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=cfcbf17c77e386b244fad41ab0dc6eec`
+        url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=cfcbf17c77e386b244fad41ab0dc6eec&units=metric&lang=pt_br`
     }
     
     fetch(url).then((response) => {
@@ -16,11 +16,12 @@ function chamarAPI(cidade, carregadoInicio = false, lat = 0, long = 0) {
 
     .then((dados) => {
         console.log(dados)
-        const temperatura = parseInt(((parseInt(dados.main.temp) - 273)));
-        const temperatura_max = parseInt(((parseInt(dados.main.temp_max) - 273)));
-        const temperatura_min = parseInt(((parseInt(dados.main.temp_min) - 273)));
-        const velocidade_vento = parseInt(dados.wind.speed)
-        const umidade = parseInt(dados.main.humidity)
+        const temperatura = parseInt(dados.main.temp);
+        const temperatura_max = parseInt(dados.main.temp_max);
+        const temperatura_min = parseInt(dados.main.temp_min);
+        const velocidade_vento = parseInt(dados.wind.speed);
+        const umidade = parseInt(dados.main.humidity);
+        const info_clima = dados.weather[0].description.replace(/(^|\s)\S/g, (L) => L.toUpperCase());
         const iconCode = dados.weather[0].icon;
         const isSunny = iconCode.includes('01d') || iconCode.includes('02d');
         const color = isSunny ? 'rgb(255, 210, 97)' : 'rgb(152, 156, 156)';
@@ -28,11 +29,12 @@ function chamarAPI(cidade, carregadoInicio = false, lat = 0, long = 0) {
         document.getElementById("temperatura_max").innerHTML = temperatura_max + "<sup>ºC</sup>";
         document.getElementById("temperatura_min").innerHTML = temperatura_min + "<sup>ºC</sup>";
         document.getElementById("vento").innerHTML = velocidade_vento + " km/h";
-        document.getElementById("umidade").innerHTML = velocidade_vento + " %";
+        document.getElementById("umidade").innerHTML = umidade + " %";
         document.getElementById("cidade").innerHTML = dados.name + ", " + dados.sys.country;
         document.getElementById("color_temp").style.backgroundColor = color;
         document.getElementById("imagem").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}@4x.png`
         document.getElementById("imagem").style.width = "8rem";
+        document.getElementById("info_clima").innerHTML = info_clima
     }).catch((e) => {
         throw new Error(e)
     })
